9f89488709c1a6cd61f47547d4abe38898e8cbc8
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2023-11-10 17:28:59 +0500

    first commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import {useRouter} from 'expo-router'
+import styles from './welcome.style'
+import { SIZES,icons } from '../../../constants';
+
+const Welcome = () => {
+  const router = useRouter();
+  return (
+    <View>
+      <View style></View>
+    </View>
+  )
+}
+
+export default Welcome
